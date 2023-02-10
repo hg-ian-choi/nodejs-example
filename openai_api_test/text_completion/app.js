0bdd3c46565615bbaabd5b70ec8b75b8a1227f8a
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-10 19:11:01 +0900

    completion: modify UI

--- openai_api_test/text_completion/app.js
+++ openai_api_test/text_completion/app.js
@@ -23,34 +23,50 @@ app.get('/', (request_, response_) => {
 });
 
 app.post('/completion', async (request_, response_) => {
-  const {words} = request_.body;
-  console.log(words);
+  const {words, histories} = request_.body;
+
+  let prompt = '';
+
+  if (histories.length > 0) {
+    for await (const history of histories) {
+      prompt += `\n\nI: ${history.question}`;
+      prompt += `\n\nGPT-3: ${histories.answer}`;
+    }
+  }
+
   try {
+    prompt += `\n\nI: ${words}`;
     const _response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: words,
-      temperature: 0.4,
-      max_tokens: 500,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
+      prompt: prompt,
+      // suffix: 'Thank you for using Griffin GPT',
+      max_tokens: 300, // [0, 4096]
+      temperature: 0, // [0.0, 2.0]
+      top_p: 0, // [0.0, 1.0]
+      n: 1,
+      stream: false,
+      logprobs: null,
+      frequency_penalty: 0, // [-2.0, 2.0]
+      presence_penalty: 0, // [-2.0, 2.0]
 
+    });
+    let result;
     if (!_response.data.choices[0].text) {
       console.log('_response.data.choices', _response.data.choices);
       // throw new Error('Failed to complete');
     }
+    result = _response.data.choices[0].text.split('GPT-3: ')
     console.log(_response.data.choices);
-    response_.status(200).send(_response.data.choices[0].text);
+    response_.status(200).send(result[1] ? `\n${result[1]}` : _response.data.choices[0].text);
   } catch (e) {
-    // console.log('error: => ', e);
     console.log('error!!!');
+    console.log('error: => ', e);
   }
 });
 
 app.post('/edit', async (request_, response_) => {
   console.log('request_.body', request_.body);
-  const response = await openai.createEdit()
+  // const response = await openai.createEdit()
 })
 
 app.listen(port, () => {
